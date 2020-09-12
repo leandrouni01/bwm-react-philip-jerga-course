137c9db094af8ebaca3909a870c83dd403292f04
Leandro Murgo <leandrouni01@gmail.com>
2020-09-12 14:57:27 -0300

    ejemplo con rjx

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import './index.css';
 
-const h2Element = React.createElement('i', null,' What a nice day');
-const helloWorldElement = React.createElement('h1', {className: 'title'}, 'Hello World', h2Element);
+//const h2Element = React.createElement('i', null,' What a nice day');
+//const helloWorldElement = React.createElement('h1', {className: 'title'}, 'Hello World', h2Element);
 const rootElement = document.getElementById('root');
 
-ReactDOM.render(helloWorldElement, rootElement);
+ReactDOM.render(<h1 className="title">Hello World <i>What a nice day</i></h1>, rootElement);
