2fe61bcbfb578ea6233e08d8a571f221954b5fc8
Leandro Murgo <leandrouni01@gmail.com>
2020-09-12 15:13:03 -0300

    Separando componente funcional

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+
+function App () {
+    return (
+        <h1 className="title">
+            Hello World <i>What a nice day</i>
+        </h1>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 
 import ReactDOM from 'react-dom';
 import React from 'react';
+import App from './App';
 import './index.css';
 
 //const h2Element = React.createElement('i', null,' What a nice day');
 //const helloWorldElement = React.createElement('h1', {className: 'title'}, 'Hello World', h2Element);
 const rootElement = document.getElementById('root');
 
-function App (props) {
-    return (
-        <h1 className={props.className}>
-            Hello World <i>What a nice day</i>
-        </h1>
-    );
-}
 
-ReactDOM.render(React.createElement(App, {className: "title"}), rootElement);
+ReactDOM.render(React.createElement(App,null,null), rootElement);
