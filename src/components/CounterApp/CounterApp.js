5f3291334b8b73c7e1ab2d2a47329e28ce4ca34b
Leandro Murgo <leandrouni01@gmail.com>
2020-09-14 00:18:27 -0300

    Segunda forma de CounterApp

--- src/components/CounterApp/CounterApp.js
+++ src/components/CounterApp/CounterApp.js
@@ -3,6 +3,7 @@
 import React , {useState} from 'react';
 import './CounterApp.css';
 
+
 const CounterApp = () => {
 
   const [count, setCount] = useState(0);
@@ -13,13 +14,7 @@ const CounterApp = () => {
   */
 
 
-  const increment = () => {
-    setCount(count + 1);
-  }
-
-  const decrement = () => {
-    setCount(count - 1);
-  }
+  const mutateCount = (step) => ()=> setCount(count + step);
 
   return (
     <div>
@@ -28,8 +23,8 @@ const CounterApp = () => {
         <h1 className="value">
           {count}
         </h1>
-        <button onClick={increment}>Increment</button>
-        <button onClick={decrement}>Decrement</button>
+        <button onClick={mutateCount(-1)}>Decrement</button>
+        <button onClick={mutateCount(1)}>Increment</button>
       </div>
     </div>
   ) 
