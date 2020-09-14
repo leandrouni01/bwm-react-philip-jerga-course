19128ee52be642ab6a0e7f9ea5de36588a00d6c9
Leandro Murgo <leandrouni01@gmail.com>
2020-09-13 21:21:15 -0300

    Primer estado de CounterApp

--- src/components/CounterApp/CounterApp.js
+++ src/components/CounterApp/CounterApp.js
@@ -1,16 +1,36 @@
 
 
-import React from 'react';
+import React , {useState} from 'react';
 import './CounterApp.css';
 
 const CounterApp = () => {
-    return (
-    <div className="counter-app">
-      <h1 className="value">
-        0
-      </h1>
-      <button>Increment</button>
-      <button>Decrement</button>
+
+  const [count, setCount] = useState(0);
+  /* this is the same as writing this:
+  * const stateArray = useState(0);
+  * const count = stateArray[0];
+  * const setCount = stateArray[1];
+  */
+
+
+  const increment = () => {
+    setCount(count + 1);
+  }
+
+  const decrement = () => {
+    setCount(count - 1);
+  }
+
+  return (
+    <div>
+      <h2>Hello World</h2>
+      <div className="counter-app">
+        <h1 className="value">
+          {count}
+        </h1>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </div>
     </div>
   ) 
 }
