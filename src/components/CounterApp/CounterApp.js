fdef11947dd222855e7dc9c2003092cfc7047e8c
Leandro Murgo <leandrouni01@gmail.com>
2020-09-14 18:46:21 -0300

    useCallback agregado y explicado

--- src/components/CounterApp/CounterApp.js
+++ src/components/CounterApp/CounterApp.js
@@ -3,13 +3,23 @@ import propTypes from "prop-types";
 import "./CounterApp.css";
 import CounterView from "./CounterView";
 
+const functions = new Set();
 
 const CounterApp = (props) => {
 
   const [count, setCount] = useState(0);
   const { title } = props;
+  const [anyNumber, setAnyNumber] = useState(99);
 
-  const mutateCount = (step) => ()=> setCount(count + step);
+
+  const mutateCount = React.useCallback((step) => ()=> setCount(count + step),[count]);
+  const changeAnyNumber = React.useCallback(()=> () => setAnyNumber(anyNumber + 1),[anyNumber]);
+
+  // React.useCallback returns the same function as a callback if the state in the didn´t change
+  // so you save memory(use in case of an overly updated function)
+
+  functions.add(mutateCount);
+  functions.add(changeAnyNumber);
 
   return (
     <div>
@@ -20,6 +30,8 @@ const CounterApp = (props) => {
         countValue={count}
         handleIncrement={mutateCount}
         />
+        <button onClick={changeAnyNumber()}>Click Me</button>
+        <h1>Functions: {functions.size} </h1>
       </div>
     </div>
   )
--- src/components/CounterApp/CounterView.js
+++ src/components/CounterApp/CounterView.js
@@ -4,6 +4,7 @@ import React , { useEffect } from "react";
 const CounterView = (props) => {
 
 const {countValue, handleIncrement} = props;
+const randomColor = () =>  '#'+(Math.random() * 0xFFFFFF<<0).toString(16);
 
 useEffect(() => {
   console.log("Effect hook used");
@@ -13,7 +14,7 @@ useEffect(() => {
   // any of the states in the passed array are changed
 
   return (
-    <div>
+    <div style={{background: randomColor()}}>
       <h2 className="value"> {countValue} </h2>
       <button onClick={handleIncrement(-1)}>Decrement</button>
       <button onClick={handleIncrement(1)}>Increment</button>
