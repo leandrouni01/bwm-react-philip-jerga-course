0a2bec4da1834bc763272d0d962c3b502c76876b
Leandro Murgo <leandrouni01@gmail.com>
2020-09-14 10:31:36 -0300

    CounterApp como componente de clase

--- src/components/CounterApp/CounterApp.js
+++ src/components/CounterApp/CounterApp.js
@@ -3,7 +3,7 @@
 import React , {useState} from 'react';
 import './CounterApp.css';
 
-
+/*
 const CounterApp = () => {
 
   const [count, setCount] = useState(0);
@@ -11,7 +11,7 @@ const CounterApp = () => {
   * const stateArray = useState(0);
   * const count = stateArray[0];
   * const setCount = stateArray[1];
-  */
+  *
 
 
   const mutateCount = (step) => ()=> setCount(count + step);
@@ -27,7 +27,41 @@ const CounterApp = () => {
         <button onClick={mutateCount(1)}>Increment</button>
       </div>
     </div>
-  ) 
+  )
+}*/
+
+class CounterApp extends React.Component {
+
+  state = {
+    count: 0
+  }
+
+  incrementCount = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  decrementCount = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Hello World</h2>
+        <div className="counter-app">
+          <h1 className="value">
+            {this.state.count}
+          </h1>
+          <button onClick={this.decrementCount}>Decrement</button>
+          <button onClick={this.incrementCount}>Increment</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default CounterApp;
