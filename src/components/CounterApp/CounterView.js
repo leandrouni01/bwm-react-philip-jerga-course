3d5a8f42deec3fdb2d400aef15dcd32bfe996753
Leandro Murgo <leandrouni01@gmail.com>
2020-09-14 13:05:54 -0300

    useEffect agregado y explicado

--- src/components/CounterApp/CounterView.js
+++ src/components/CounterApp/CounterView.js
@@ -1,8 +1,16 @@
-import React from "react";
+
+import React , { useEffect } from "react";
 
 const CounterView = (props) => {
 
 const {countValue, handleIncrement} = props;
+
+useEffect(() => {
+  console.log("Effect hook used");
+},[countValue]);
+
+  // Use effect works as the lifecicle functions of a class component and is called at start and when 
+  // any of the states in the passed array are changed
 
   return (
     <div>
