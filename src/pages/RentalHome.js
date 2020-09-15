d23d5e9d18ea90a7ea93b369ea75a0e20c3c2134
Leandro Murgo <leandrouni01@gmail.com>
2020-09-15 14:53:28 -0300

    Agregados ejemplo de data store

--- src/pages/RentalHome.js
+++ src/pages/RentalHome.js
@@ -1,46 +1,17 @@
 import React from "react";
 import RentalCard from "../components/rental/RentalCard";
+import store from '../store'
 
 class RentalHome extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      rentals: [
-        {
-          _id: "1",
-          title: "Nice view on ocean",
-          city: "San Francisco",
-          category: "condo",
-          image: "http://via.placeholder.com/350x250",
-          numOfRooms: 4,
-          shared: true,
-          description: "Very nice apartment in center of the city.",
-          dailyPrice: 43,
-        },
-        {
-          _id: "2",
-          title: "Modern apartment in center",
-          city: "New York",
-          category: "apartment",
-          image: "http://via.placeholder.com/350x250",
-          numOfRooms: 1,
-          shared: false,
-          description: "Very nice apartment in center of the city.",
-          dailyPrice: 11,
-        },
-        {
-          _id: "3",
-          title: "Old house in nature",
-          city: "Bratislava",
-          category: "house",
-          image: "http://via.placeholder.com/350x250",
-          numOfRooms: 5,
-          shared: true,
-          description: "Very nice apartment in center of the city.",
-          dailyPrice: 23,
-        },
-      ],
-    };
+
+  state = {
+    rentals: []
+  }
+
+  componentDidMount() {
+    this.setState({
+      rentals: store.rentals()
+    });
   }
 
   renderRentals = rentals => 
@@ -52,7 +23,7 @@ class RentalHome extends React.Component {
   
 
   render() {
-    const { rentals } = this.state;
+    const  { rentals }  = this.state;
 
     return (
       <div className="card-list">
