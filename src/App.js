02f94ed11c93e2b14cc15c686e7bae9fa6f05b16
Leandro Murgo <leandrouni01@gmail.com>
2020-09-15 10:37:04 -0300

    Creado pagina de inicio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import React from 'react';
 import Header from './components/shared/Header'
+import RentalHome from './pages/RentalHome';
 
 const App = () => {
     return (
         <div>
             <Header />
+            <RentalHome />
         </div>
     );
 }
