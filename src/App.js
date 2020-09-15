0575c3e79e16cade03c88757616277fbc636b0ec
Leandro Murgo <leandrouni01@gmail.com>
2020-09-15 11:55:40 -0300

    Agregado sistema simple de navegacion(a modo de ejemplo)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@
 import React from 'react';
 import Header from './components/shared/Header'
 import RentalHome from './pages/RentalHome';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 
 const App = () => {
+
+    const renderPages = () => {
+        const {pathname} = window.location;
+
+        switch (pathname) {
+            case '/': 
+                return <RentalHome />;
+            case '/Login': 
+                return <Login />;
+            case '/Register': 
+                return <Register />
+            default: 
+                return <RentalHome />;
+        }
+    }
+
     return (
         <div>
             <Header />
-            <RentalHome />
+            {renderPages()}
         </div>
     );
 }
---
+++ src/pages/Register.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+class Register extends React.Component {
+
+
+    render() {
+        return (
+            <h1>I am A Register Page</h1>
+        )
+    }
+}
+
+export default Register;
