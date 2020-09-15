d11a81650317d2be78c2e630df39fe8b340f9e9b
Leandro Murgo <leandrouni01@gmail.com>
2020-09-15 03:16:37 -0300

    Agregados estilos y uso de scss

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+
+import './index.scss';
 import 'bootstrap/dist/js/bootstrap.js'
 
 const rootElement = document.getElementById('root');
