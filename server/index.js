e52822c99a89ad73636284133ee6e1a6faefe405
Leandro Murgo <leandrouni01@gmail.com>
2020-09-24 11:09:24 -0300

    Agregados metodos patch y delete para rentals

--- server/index.js
+++ server/index.js
@@ -7,26 +7,63 @@ const PORT = process.env.PORT || 3001;
 
 const rentals = [
   { _id: "DG2D4F3SS54", city: "New York", title: "Very nice place" },
-  { _id: "G21G3S5354D", city: "Berlin", title: "Very nice place as well!" }
+  { _id: "G21G3S5354D", city: "Berlin", title: "Very nice place as well!" },
 ];
 
 app.use(bodyParser.json());
 
-app.get('/api/v1/rentals', (req, res) => {
-    return res.json(rentals);
+app.get("/api/v1/rentals", (req, res) => {
+  return res.json(rentals);
 });
 
-app.get('/api/v1/rentals/:rentalId', (req, res) => {
-    const rental = rentals.find( r => r._id === req.params.rentalId);
+app.get("/api/v1/rentals/:rentalId", (req, res) => {
+  const rental = rentals.find((r) => r._id === req.params.rentalId);
 
-    return res.json(rental);
+  return res.json(rental);
 });
 
-app.post('/api/v1/rentals', (req,res) => {
+app.delete("/api/v1/rentals/:rentalId", (req, res) => {
+  const rIndex = rentals.findIndex(
+    (rental) => rental._id === req.params.rentalId
+  );
+  if (rIndex != -1) {
+    const deletedRental = rentals.splice(rIndex, 1)[0];
+    return res.json({
+      message: `The rental with id of ${deletedRental._id} was deleted`,
+    });
+  } else {
+    return res.json({
+      message: `The rental with id of ${req.params.rentalId} was not found`,
+    });
+  }
+});
+
+app.patch("/api/v1/rentals/:rentalId", (req, res) => {
+  const { rentalId } = req.params;
+  const rIndex = rentals.findIndex((rental) => rental._id === rentalId);
+  if (rIndex != -1) {
+    const newRental = req.body;
+    if (newRental.city) {
+      rentals[rIndex].city = newRental.city;
+    }
+    if (newRental.title) {
+      rentals[rIndex].title = newRental.title;
+    }
+    return res.json({
+      message: `The rental with id of ${rentalId} was updated`,
+    });
+  } else {
+    return res.json({
+      message: `The rental with id of ${rentalId} was not found`,
+    });
+  }
+});
+
+app.post("/api/v1/rentals", (req, res) => {
   const rentalData = req.body;
   rentals.push(rentalData);
 
-  return res.json({message: `Rental with id: ${rentalData._id} was added`});
+  return res.json({ message: `Rental with id: ${rentalData._id} was added` });
 });
 
 app.listen(PORT, () => {
