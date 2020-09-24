323c205e2de458605a8c985e583490376d8076d0
Leandro Murgo <leandrouni01@gmail.com>
2020-09-24 19:56:53 -0300

    Agregado mongoose como object modeling tool y creado cluster en mongobd Atlas

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const mongoose = require('mongoose');
+
 const rentalRoutes = require('./routes/rentals');
 
 const app = express();
-
 const PORT = process.env.PORT || 3001;
+const DBURI = 'mongodb+srv://test:testtest@bwm-new.wlkrb.mongodb.net/bwm-new-dev?retryWrites=true&w=majority';
+
+mongoose.connect(DBURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}, () => {
+  console.log("Connected to DB");
+});
 
 //Midlewares
 app.use(bodyParser.json());
