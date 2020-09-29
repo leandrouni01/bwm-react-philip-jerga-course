c85d593407752226e32de67c990d1475ce887813
Leandro Murgo <leandrouni01@gmail.com>
2020-09-29 13:24:07 -0300

    Agrgada clase auxiliar para reiniciar la base de datos

---
+++ server/fakeDB/data/rentals.js
@@ -0,0 +1,37 @@
+const rentals = [
+  {
+    title: "Central Apartment 3",
+    city: "bratislava",
+    street: "hlavna",
+    category: "condo",
+    image: "http://via.placeholder.com/350x250",
+    numOfRooms: 2,
+    shared: true,
+    description: "Very Nice apartment",
+    dailyPrice: 223,
+  },
+  {
+    title: "Central House 1",
+    city: "bratislava",
+    street: "hlavna",
+    category: "house",
+    image: "http://via.placeholder.com/350x250",
+    numOfRooms: 3,
+    shared: false,
+    description: "Very Nice",
+    dailyPrice: 224,
+  },
+  {
+    title: "Central Apartment 1",
+    city: "bratislava",
+    street: "hlavna",
+    category: "condo",
+    image: "http://via.placeholder.com/350x250",
+    numOfRooms: 3,
+    shared: true,
+    description: "Also Very Nice",
+    dailyPrice: 224,
+  }
+];
+
+module.exports = rentals;
